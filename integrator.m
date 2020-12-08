a9c86f87476811d5061434b5f7093bf5c024c8ce
scottlim0102 <52173059+scottlim0102@users.noreply.github.com>
2020-12-08 18:08:05 +0900

    First draft of integration. Still needs a lot of changes

--- integrator.m
+++ integrator.m
@@ -4,18 +4,24 @@ function [x,y,z] = integrator(matrix, n)
     %   n: The desired n_th joint
     
     % Outputs:
-    %   x, y, z: The integrated component of the input matrix
+    %   x, y, z: The cumulative integrated component of the input matrix
     
     % Currently, just looking at translational part and neglecting
-    % rotational part. We might need to address rotation.
+    % rotational part. We might need to address rotation also.
+    % Also, we need to address the indexing (ex. Velocity has 931 data
+    % points and cumtrapz produce 930 usable data, while position has 932
+    % points) and constant.
     
     dx = squeeze(matrix(:,n,1,4));
     x = cumtrapz(dx);
+    x = x * 16.67e-4; % Adjust from unit time(1sec) to actual interval(16.67ms)
     
     dy = squeeze(matrix(:,n,1,4));
     y = cumtrapz(dx);
+    y = y * 16.67e-4; % Adjust from unit time(1sec) to actual interval(16.67ms)
     
     dz = squeeze(matrix(:,n,1,4));
     z = cumtrapz(dx);
+    z = z * 16.67e-4; % Adjust from unit time(1sec) to actual interval(16.67ms)
     
 end
