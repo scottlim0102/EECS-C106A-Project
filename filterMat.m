0396196746567ac34b408e11d11cbba17652247b
Brian <huoshengyu@berkeley.edu>
2020-12-09 00:06:35 -0800

    filter overhaul, deriveJoints reformat, my_PLot2 for cell arrays added, butterMat and filterMat fix for positional vector interpretation

--- filterMat.m
+++ filterMat.m
@@ -1,9 +1,9 @@
-function butter_mat = butterMat(Joint_Mat)
+function filter_mat = filterMat(Joint_Mat)
 % Applies Butterworth filter to Joint_Mat
 % Should output a filtered M timestamps x N joints cell array with 4 x 4
 % transformation matrix entries.
-    fo = 6;     % Order of the filter. Higher gives steeper cutoffs.
-    fc = 3;     % Cutoff frequency in Hz
+    fo = 1;     % Order of the filter. Higher gives steeper cutoffs.
+    fc = 1;     % Cutoff frequency in Hz
     fs = 60;    % Sample rate in Hz
     [b,a] = butter(fo,fc/(fs/2)); % [b,a] each of length = fo + 1
     % freqz(b,a) % Uncomment to see filter response
@@ -18,17 +18,18 @@ function butter_mat = butterMat(Joint_Mat)
             vectors{i,j} = Joint_Mat{i,j}(1:3,4).';
         end
     end
-    butter_thetas = filter(b,a,cell2mat(thetas));
-    butter_vectors = filter(b,a,cell2mat(vectors));
-    butter_rotmats = cell(mat_size(1), mat_size(2));
-    butter_mat = cell(mat_size(1), mat_size(2));
+    cell2mat(vectors)
+    filter_thetas = filter(b,a,cell2mat(thetas));
+    filter_vectors = filter(b,a,cell2mat(vectors));
+    filter_rotmats = cell(mat_size(1), mat_size(2));
+    filter_mat = cell(mat_size(1), mat_size(2));
     for j = 1:mat_size(2)
         for i = 1:mat_size(1)
-            butter_rotmats{i,j} = eul2rotm(butter_thetas(i,3*j-2:3*j));
-            butter_mat{i,j} = zeros(4,4);
-            butter_mat{i,j}(1:3,1:3) = butter_rotmats{i,j};
-            butter_mat{i,j}(1:3,4) = butter_vectors(i,3*j-2:3*j);
-            butter_mat{i,j}(4,4) = 1;
+            filter_rotmats{i,j} = eul2rotm(filter_thetas(i,3*j-2:3*j));
+            filter_mat{i,j} = zeros(4,4);
+            filter_mat{i,j}(1:3,1:3) = filter_rotmats{i,j};
+            filter_mat{i,j}(1:3,4) = filter_vectors(i,3*j-2:3*j);
+            filter_mat{i,j}(4,4) = 1;
         end
     end
     
